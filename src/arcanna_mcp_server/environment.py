f40751eca2fe6131e9891b986c0aba6a6a8f8ca4
Alin Manolache <alinmanolache2005@gmail.com>
2025-03-28 18:50:22 +0200

    add generic API (v2), remove input related API (v1)

--- src/arcanna_mcp_server/environment.py
+++ src/arcanna_mcp_server/environment.py
@@ -1,7 +1,6 @@
 import os
 
 MANAGEMENT_API_KEY = os.getenv("ARCANNA_MANAGEMENT_API_KEY")
-API_KEY = os.getenv("ARCANNA_INPUT_API_KEY")
 ARCANNA_HOST = os.getenv("ARCANNA_HOST", "")
 ARCANNA_USER = "MCP" + "-" + (os.getenv("ARCANNA_USER") or "user")
 TRANSPORT_MODE = os.getenv("TRANSPORT_MODE", "stdio")
@@ -10,8 +9,6 @@ TRANSPORT_MODE = os.getenv("TRANSPORT_MODE", "stdio")
 def validate_environment_variables():
     if MANAGEMENT_API_KEY is None:
         raise Exception("Arcanna management api key not found")
-    if API_KEY is None:
-        raise Exception("Arcanna input API KEY not found.")
 
     if ARCANNA_HOST is None:
         raise Exception("Arcanna HOST not found.")
--- src/arcanna_mcp_server/tools/__init__.py
+++ src/arcanna_mcp_server/tools/__init__.py
@@ -1,7 +1,6 @@
 import arcanna_mcp_server.tools.resources
 from mcp.server import FastMCP
 import arcanna_mcp_server.tools.jobs
-import arcanna_mcp_server.tools.events
 import arcanna_mcp_server.tools.health_check
 import arcanna_mcp_server.tools.custom_code_block
 import arcanna_mcp_server.tools.generic_events
@@ -10,7 +9,6 @@ import arcanna_mcp_server.tools.generic_events
 def attach_tools(mcp_server: FastMCP):
     modules = [
         arcanna_mcp_server.tools.jobs,
-        arcanna_mcp_server.tools.events,
         arcanna_mcp_server.tools.health_check,
         arcanna_mcp_server.tools.custom_code_block,
         arcanna_mcp_server.tools.resources,
