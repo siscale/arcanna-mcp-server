010f9581a8ded880a267351de5475d7baeac3009
Alin Manolache <alinmanolache2005@gmail.com>
2025-04-02 12:59:11 +0300

    rm unused ARCANNA_USER

--- src/arcanna_mcp_server/environment.py
+++ src/arcanna_mcp_server/environment.py
@@ -1,8 +1,8 @@
 import os
 
+
 MANAGEMENT_API_KEY = os.getenv("ARCANNA_MANAGEMENT_API_KEY")
 ARCANNA_HOST = os.getenv("ARCANNA_HOST", "")
-ARCANNA_USER = "MCP" + "-" + (os.getenv("ARCANNA_USER") or "user")
 TRANSPORT_MODE = os.getenv("TRANSPORT_MODE", "stdio")
 
 
