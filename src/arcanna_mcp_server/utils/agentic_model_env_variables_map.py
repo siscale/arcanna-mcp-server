e8307995cfb0a4e3b5bae87b434382527c5f1406
DenisAndreiStefan <dstefan@siscale.com>
2026-05-04 10:34:08 +0300

    remove unused dict

--- src/arcanna_mcp_server/utils/agentic_model_env_variables_map.py
+++ src/arcanna_mcp_server/utils/agentic_model_env_variables_map.py
@@ -3,25 +3,6 @@ from typing import List, Optional
 
 from arcanna_mcp_server.models.agentic.env_variable import EnvVariable
 
-agentic_model_env_variables_map = {
-    "bedrock": {
-        "AWS_ACCESS_KEY_ID": os.getenv("AWS_ACCESS_KEY_ID"),
-        "AWS_SECRET_ACCESS_KEY": os.getenv("AWS_SECRET_ACCESS_KEY"),
-        "AWS_REGION_NAME": os.getenv("AWS_REGION_NAME", "us-east-1")
-    },
-    "openai": {
-        "OPENAI_API_KEY": os.getenv("OPENAI_API_KEY"),
-        "OPENAI_API_BASE": os.getenv("OPENAI_API_BASE"),
-    },
-    "anthropic": {
-        "ANTHROPIC_API_KEY": os.getenv("ANTHROPIC_API_KEY")
-    },
-    "vllm": {
-        "HOSTED_VLLM_API_KEY": os.getenv("VLLM_API_KEY"),
-        "HOSTED_VLLM_API_BASE": os.getenv("VLLM_API_BASE"),
-    }
-}
-
 
 def parse_env_variables_request(env_variables: Optional[List[EnvVariable]]):
     if env_variables is None:
